f5b7411b2671ed2f808de0aea6602804aa479da2
vladimir-martin <vladimir.martin@udea.edu.co>
2023-09-29 12:22:13 -0500

    se termina exploratorio categorias

--- b_exploracion.py
+++ b_exploracion.py
@@ -31,15 +31,35 @@ ratings.head()#vision general
 ratings.info()#tipo de cada variable
 ratings.duplicated().sum()#verificar duplicados
 
+#Vemos si las tablas tienen valores nulos.
+print(movies.isnull().sum())
+print(ratings.isnull().sum())
+
+#En el dataframe ratings se cambia el formato de timestamp por uno de fecha para hacer su manejo mas cómodo.
+ratings['time'] =pd.to_datetime(ratings['timestamp'], unit='s')
+ratings.drop(columns=["timestamp"],inplace=True)
+ratings.head()
+#llevarla a sql
+ratings.to_sql("ratings_split",conn,if_exists="replace")
+pd.read_sql("""select * from ratings_split""",conn)
+
+
 
 #separar generos en db movies
 genres=movies['genres'].str.split('|')
 te = TransactionEncoder()
 genres = te.fit_transform(genres)
 genres = pd.DataFrame(genres, columns = te.columns_)
+
+#extraemos año de estreno del titulo
+movies['movyear'] = movies['title'].str[-5:-1]
+movies=movies[movies["movyear"].str.contains ('r On') == False ]
+
+#creamos nueva base con esta info
 movies_split=pd.concat([movies,genres],axis=1).drop(["genres"],axis=1)
 #llevarla a sql
 movies_split.to_sql("movies_split",conn,if_exists="replace")
+pd.read_sql("""select * from movies_split""",conn)
 
 #verificar categorias por errores tipograficos o categorias similares
 #generos de peliculas
@@ -47,29 +67,73 @@ genres.columns.tolist()
 #ratings
 np.sort(ratings["rating"].unique())
 
+
+
+##Analisis de categorias
+#se pretende saber si hay categorias que por su distribucion no aporten a los objetivos de la solucion y deban ser recortadas
+
 #base de datos de peliculas
-# Cantidad de peliculas por genero
+# Cantidad de peliculas por genero (usando python, todos los generos al tiempo)
 gen_total=pd.DataFrame(movies_split.drop(["title","movieId"],axis=1).sum()).reset_index()
 gen_total.columns=["Genre","Qty"]
 gen_total=gen_total.sort_values(by="Qty",ascending=False)
+
+#grafico
 fig  = px.bar(gen_total, x= 'Genre',y="Qty",
               title= 'Numero de peliculas por genero',
               labels={'Genre':'Genero'})
 fig.show()
 
+# Cantidad de peliculas por genero (usando sql, se debe especificar que genero)
+gen_sum=pd.read_sql("""SELECT SUM(Comedy) AS Qty_Comedy, SUM(Action) AS Qty_Action, SUM(Animation) AS Qty_Animation
+FROM movies_split""",conn)
+
+#vemos que todos los generos aportan al analisis, con una cantidad de datos significativa por genero y eliminar los generos con pocas calificaciones, no aporta tampoco a la eficiencia del codigo.
+
 #Base de datos de calificaciones
 #cantidad de calificaciones por usuario
 rating_user=pd.read_sql(''' select "userId" as User_Id,
                          count(*) as Qty
-                         from ratings
+                         from ratings_split
                          group by "userId"
                          order by Qty asc
                          ''',conn )
-fig  = px.histogram(rating_user, x= 'Qty', title= 'Hist frecuencia de numero de calificaciones por usario')
+fig  = px.histogram(rating_user, x= 'Qty', title= 'Hist frecuencia de numero de calificaciones por usuario')
 fig.show() 
 rating_user.describe()
-# no vemos necesario eliminar usuarios, ya que el minimo de calificaciones por usuario el 20 que es un buen numero y solo un 25% de los usuarios tienen mas de 168 calificaciones.
+
+#ver rango de fechas de las calificaciones
+
+pd.read_sql("""select min(strftime('%Y',time)) as año_min, max(strftime('%Y',time)) as año_max,max(strftime('%Y',time))-min(strftime('%Y',time)) as rango_años from ratings_split""",conn)
+
+
+# no vemos necesario eliminar usuarios, ya que el minimo de calificaciones por usuario es 20 que es un buen numero y solo un 25% de los usuarios tienen mas de 168 calificaciones, con datos atipicos que suben el promedio a 165 calificaciones por usuario.
+
+#cantidad de calificaciones por pelicula
+rating_movie=pd.read_sql(''' select "movieId" as Movie_Id,
+                         count(*) as Qty
+                         from ratings_split
+                         group by "movieId"
+                         order by Qty asc
+                         ''',conn )
+fig  = px.histogram(rating_movie, x= 'Qty', title= 'Hist frecuencia de numero de calificaciones por pelicula')
+fig.show() 
+rating_movie.describe()
+#no vemos necesario eliminar peliculas, la mitad de ellas tienen menos de 3 calificaciones y solo un 25% tienen mas de nuevo, con datos atipicos de un maximo de 329 calificaciones que elevan el promedio a 10 calificaciones por pelicula.
+
+
+#analisis adicionales
+
+#Número de usuarios que calificaron títulos.
+pd.read_sql("""select count(distinct(userId)) as cantidad_usuarios from ratings_split""", conn)
 
 
 
+#tabla completa creada en sql
+fn.ejecutar_sql('preprocesamiento.sql', cursor)
 
+cursor.execute("select name from sqlite_master where type='table' ")
+cursor.fetchall()
+
+#tabla completa
+pd.read_sql("""select * from full_tabla""",conn)
---
+++ preprocesamiento.sql
@@ -0,0 +1,11 @@
+drop table if exists full_tabla ;
+
+create table full_tabla as select 
+a.*,
+b.userId,
+b.rating,
+b.time
+ from movies_split a 
+ inner join
+ ratings_split b on a.movieId=b.movieId;
+ 
